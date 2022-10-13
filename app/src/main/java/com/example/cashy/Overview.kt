3009139e1ff23bd242c9944b1e3f6f83828d94fb
Emil Åkerman <emil@Emils-MacBook-Air.local>
2022-10-13 13:51:33 +0200

    recyclerview and other stuff

---
+++ app/src/main/java/com/example/cashy/Overview.kt
@@ -0,0 +1,11 @@
+package com.example.cashy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Overview : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_overview)
+    }
+}
