6a1214e6620282b35e1aad195081e7c97b759101
Emil Åkerman <emil@emils-air.lan>
2022-10-21 11:05:56 +0200

    fixed total amount read correctly

--- app/src/main/java/com/example/cashy/Overview.kt
+++ app/src/main/java/com/example/cashy/Overview.kt
@@ -1,11 +1,12 @@
 package com.example.cashy
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -13,9 +14,9 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.getField
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
-import kotlin.math.log
 
 class Overview : AppCompatActivity() {
 
@@ -25,6 +26,8 @@ class Overview : AppCompatActivity() {
     lateinit var db : FirebaseFirestore
     private lateinit var auth: FirebaseAuth
 
+    var totalSum : Int = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,20 +35,21 @@ class Overview : AppCompatActivity() {
         supportActionBar?.hide()
 
         //starting up the firestore database and authentication
-
         db = Firebase.firestore
         auth = Firebase.auth
 
-
         //finding the current user and assigning a variable to the user id
-
         val user = Firebase.auth.currentUser
         var uid = user?.uid
 
-
-
+        //reads data and populates the recyclerview (also enables the recyclerview)
         readFrom()
+        //reads total SUM data and adds to total sum up top
+        readTotalSum()
 
+
+
+        //temporary logout button
         val logoutButton = findViewById<Button>(R.id.logoutButton)
         logoutButton.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
@@ -69,14 +73,11 @@ class Overview : AppCompatActivity() {
             db.collection("users").document(user.uid).collection("receipts")
                 .get()
                     .addOnSuccessListener { documentSnapshot ->
-
                         val expenses = mutableListOf<Expense>()
-
-                        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-                        recyclerView.layoutManager = LinearLayoutManager(this)
-                        val adapter = ExpenseRecycleAdapter(this, expenses)
-                        recyclerView.adapter = adapter
-
+                            val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+                            recyclerView.layoutManager = LinearLayoutManager(this)
+                            val adapter = ExpenseRecycleAdapter(this, expenses)
+                            recyclerView.adapter = adapter
                     for (document in documentSnapshot.documents) {
                         val item = document.toObject<Expense>()
                         if (item != null) {
@@ -86,8 +87,33 @@ class Overview : AppCompatActivity() {
                 }
         }
     }
-       fun toAddReceiptButton(){
-           val resultButtonIntent = Intent(/* packageContext = */ this, /* cls = */ AddReceipt::class.java)
-            startActivity(resultButtonIntent)
+       fun toAddReceiptButton() {
+           val resultButtonIntent =
+               Intent(/* packageContext = */ this, /* cls = */ AddReceipt::class.java)
+           startActivity(resultButtonIntent)
+       }
+    //reads total sum of all fields correctly and populates the top total sum textview
+    private fun readTotalSum() {
+        val user = auth.currentUser
+        if (user != null) {
+            db.collection("users").document(user.uid).collection("receipts")
+                .get()
+                .addOnSuccessListener { documentSnapshot ->
+                    for (document in documentSnapshot.documents) {
+                        val item = document.toObject<Expense>()
+                        if (item != null) {
+                            val totalspent_txt = findViewById<TextView>(R.id.totalSpent_txt)
+                            val expenses = mutableListOf<Expense>()
+                            expenses.add(item)
+                            totalSum += item.sum!!
+                            totalspent_txt.text = totalSum.toString()
+                        }
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.w("!!!", "Error getting documents: ", exception)
+                }
+        }
+
     }
 }
