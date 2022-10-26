05901deb51b0cfb462c51998048ce48b97e35723
Emil Åkerman <emil@emils-air.lan>
2022-10-26 21:29:03 +0200

    removed old Expense class

--- app/src/main/java/com/example/cashy/Expense.kt
+++
@@ -1,8 +0,0 @@
-package com.example.cashy
-
-import com.google.firebase.firestore.DocumentId
-
-class Expense(var sum : Int? = null,
-              var category : String? = null,
-              var paymentmethod : String? = null,
-              var company : String? = null) {}
